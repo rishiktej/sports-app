e1ea9169ebf4e2b99b9e48c05293fd3126d9d41d
rishiktej <ugs21032_cic.tej@cbit.org.in>
2023-10-25 18:34:34 +0530

    added some features

--- src/pages/landingpage/livescores.tsx
+++ src/pages/landingpage/livescores.tsx
@@ -1,47 +1,103 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useMatchState } from "../../context/livescores/context";
+import { API_ENDPOINT } from "../../config/constants";
+import { match } from "../../context/livescores/types";
 
 export default function MatchDataListItems() {
   const state = useMatchState();
   const { matches, isLoading, isError, errorMessage } = state;
+  const [selectedMatches, setSelectedMatches] = useState<match[]>([]);
 
-  if (isLoading) {
-    return <span>Loading...</span>;
-  }
+  const fetchMatchData = async (matchId: number) => {
+    const token = localStorage.getItem("authToken") || "";
+    try {
+      const response = await fetch(`${API_ENDPOINT}/matches/${matchId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
-  if (isError) {
-    return <span>{errorMessage}</span>;
-  }
+      if (!response.ok) {
+        throw new Error("Failed to fetch Match Data");
+      }
 
-  // Sort matches by their 'endsAt' timestamp in descending order (latest first)
-  const sortedMatches = [...matches].sort(
-    (a, b) => new Date(b.endsAt).getTime() - new Date(a.endsAt).getTime()
-  );
+      const data = await response.json();
+      setSelectedMatches((prevMatches) =>
+        prevMatches.map((prevMatch) =>
+          prevMatch.id === data.id ? data : prevMatch
+        )
+      );
+    } catch (error) {
+      console.error("Operation failed:", error);
+    }
+  };
 
-  // Take the latest five matches
-  const latestMatches = sortedMatches.slice(0, 5);
+  useEffect(() => {
+    // Fetch match data for each of the latest matches
+    const latestMatchIds = matches
+      .sort(
+        (a, b) => new Date(b.endsAt).getTime() - new Date(a.endsAt).getTime()
+      )
+      .slice(0, 5)
+      .map((match) => match.id);
+
+    // Fetch data for each of the latest matches
+    latestMatchIds.forEach((matchId) => {
+      fetchMatchData(matchId);
+    });
+  }, []);
+
+  const handleRefreshClick = (matchId: number) => {
+    // Manually refresh the scores for a specific match
+    fetchMatchData(matchId);
+  };
 
   return (
     <div className="flex space-x-4 overflow-x-auto">
-      {latestMatches.map((match) => (
+      {selectedMatches.map((match) => (
         <div
           key={match.id}
           className="flex-shrink-0 w-64 p-4 bg-white border border-gray-200 rounded-lg shadow hover:shadow-lg dark:bg-gray-800"
         >
           <p className="text-gray-600 text-sm dark:text-gray-400">
-            sport:{match.sportName}
+            Sport: {match.sportName}
           </p>
           <h5 className="mb-2 text-sm font-medium tracking-tight text-gray-900 dark:text-white">
             {match.name}
           </h5>
-          <p className="text-gray-600 text-sm dark:text-gray-400">
-            venue:{match.location}
-          </p>
-          <p className="text-gray-600 text-sm dark:text-gray-400"></p>
-          <p className="text-gray-600 text-sm dark:text-gray-400">
+          <div className="mb-4">
+            <h5 className="text-sm font-medium text-gray-900 dark:text-white">
+              SCORE:
+            </h5>
+            <ul className="list-decimal pl-4">
+              <ol className="list-decimal pl-4">
+                {Object.entries(match.score).map(([team, score]) => (
+                  <li
+                    key={team}
+                    className="text-sm text-gray-600 dark:text-gray-400"
+                  >
+                    {team}: {score}
+                  </li>
+                ))}
+              </ol>
+            </ul>
+          </div>
+          <div className="flex items-center">
+            <p className="text-sm text-gray-600 dark:text-gray-400">
+              Venue: {match.location}
+            </p>
+          </div>
+          <p className="text-sm text-gray-600 dark:text-gray-400">
             Ends at: {new Date(match.endsAt).toLocaleString()}
           </p>
-          {/* Add more properties you want to display */}
+          <button
+            onClick={() => handleRefreshClick(match.id)}
+            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+          >
+            Refresh Score
+          </button>
         </div>
       ))}
     </div>
